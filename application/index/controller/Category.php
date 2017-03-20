715010f57306a1af996d9fb79611d0a7da7c35e6
mfanweb <530385371@qq.com>
2017-03-20 17:27:16 +0800

    添加模版

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -3,15 +3,14 @@ namespace app\index\controller;
 
 use think\Controller;
 
-class Index extends Controller
+class Category extends Controller
 {
     public function index()
     {
        return $this->fetch();
     }
-    public function welcome(){
+    public function add(){
         return $this->fetch();
     }
-
 
 }
